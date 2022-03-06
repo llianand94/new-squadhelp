a027457b74359f0893583cd2f8e0872054afb473
Andrey <llianandr94@gmail.com>
2022-03-06 21:54:57 +0200

    Initial commit and fixed all start warnings

---
+++ client/src/api/ws/socketController.js
@@ -0,0 +1,11 @@
+import ChatSocket from './sockets/ChatSocket';
+import NotificationSocket from './sockets/NotificationSocket';
+
+export let controller;
+export let chatController;
+
+export const initSocket = (store) => {
+  controller = new NotificationSocket(store.dispatch, store.getState, 'notifications');
+  chatController = new ChatSocket(store.dispatch, store.getState, 'chat');
+  return store;
+};
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,42 @@
+import ACTION from '../actions/actionTypes';
+
+const initialState = {
+  isFetching: false,
+  error: null,
+};
+
+function authReducer (state = initialState, action) {
+  switch (action.type) {
+    case ACTION.AUTH_ACTION_REQUEST: {
+      return {
+        isFetching: true,
+        error: null,
+      };
+    }
+    case ACTION.AUTH_ACTION_SUCCESS: {
+      return {
+        isFetching: false,
+        error: null,
+      };
+    }
+    case ACTION.AUTH_ACTION_ERROR: {
+      return {
+        isFetching: false,
+        error: action.error,
+      };
+    }
+    case ACTION.AUTH_ACTION_CLEAR_ERROR: {
+      return {
+        ...state,
+        error: null,
+      };
+    }
+    case ACTION.AUTH_ACTION_CLEAR: {
+      return initialState;
+    }
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
